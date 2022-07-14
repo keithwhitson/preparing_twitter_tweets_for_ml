eb723d82d248b808b6f262980859dd701ba33d46
Keith Whitson <keith.whitson@franka.de>
2022-07-14 22:51:52 +0200

    added2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from spacy.language import Language
 from spacy_langdetect import LanguageDetector
 import spacy
@@ -65,16 +66,62 @@ def get_retweets_count(tweet):
     retweets = tweet.count('RT ')
     return retweets
 
+
 def get_links_count(tweet):
     '''Get the number of links'''
     links = tweet.count('http')
     return links
 
-for tweet in ['Shot 60 times. Armed with nothing but his Blac...']:
-    print('language: ', language_detection(tweet))
-    print('cleaned_tweet: ', cleaned_tweet(tweet))
-    print('vader_sent: ', vader_sent(tweet))
-    print('hashtag_count: ', get_hashtag_count(tweet))
-    print('retweets_count:', get_retweets_count(tweet))
-    print('mentions_count: ', get_mentions_count(tweet))
-    print('links_count: ', get_links_count(tweet))
+
+def get_num_words_dirty(tweet):
+    '''Returns the num of words before cleaning'''
+    word_list = tweet.split()
+    number_of_words = len(word_list)
+    return number_of_words
+
+
+def get_num_words_cleaned(tweet):
+    '''Returns the number of words in the cleaned tweet'''
+    removed_link_tweet = re.sub(r"http\S+", "", tweet)
+    removed_mention_tweet = re.sub("@[A-Za-z0-9]+", "", removed_link_tweet)
+    removed_non_alpha = re.sub(r'[\W_]+', ' ', removed_mention_tweet)
+    removed_rt = removed_non_alpha.replace('RT ', '')
+    single_spaced_tweet = " ".join(removed_rt.split())
+    lowercased_tweet = single_spaced_tweet.lower()
+    word_list = lowercased_tweet.split()
+    number_of_words = len(word_list)
+    return number_of_words
+
+
+def get_num_chars_cleaned(tweet):
+    removed_link_tweet = re.sub(r"http\S+", "", tweet)
+    removed_mention_tweet = re.sub("@[A-Za-z0-9]+", "", removed_link_tweet)
+    removed_non_alpha = re.sub(r'[\W_]+', ' ', removed_mention_tweet)
+    removed_rt = removed_non_alpha.replace('RT ', '')
+    single_spaced_tweet = " ".join(removed_rt.split())
+    return len(single_spaced_tweet)
+
+
+def get_num_chars_dirty(tweet):
+    return len(tweet)
+
+# df = (pd.read_parquet('all_black_twitter_tweets'))
+
+# df['vader_sent'] = df['tweet'].apply(vader_sent)
+# df['language_detection'] = df['tweet'].apply(language_detection)
+# df['cleaned_tweet'] = df['tweet'].apply(cleaned_tweet)
+# df['hashtag_count'] = df['tweet'].apply(get_hashtag_count)
+# df['mentions_count'] = df['tweet'].apply(get_mentions_count)
+# df['retweets_count'] = df['tweet'].apply(get_retweets_count)
+# df['links_count'] = df['tweet'].apply(get_links_count)
+# df['num_words_cleaned'] = df['tweet'].apply(get_num_words_cleaned)
+# df['num_words_dirty'] = df['tweet'].apply(get_num_words_dirty)
+# df['ratio_words_used_over_total'] = df['num_words_cleaned']/df['num_words_dirty']
+# df['num_chars_cleaned'] = df['tweet'].apply(get_num_chars_cleaned)
+# df['num_chars_dirty'] = df['tweet'].apply(get_num_chars_dirty)
+# df['cleanliness_ratio'] = df['num_chars_cleaned']/df['num_chars_dirty']
+
+# df.to_parquet('all_black_twitter_tweets_for_ml.parquet', engine='pyarrow',partition_cols=['twitterhandle'])
+
+
+print(pd.read_parquet('all_black_twitter_tweets_for_ml.parquet'))
